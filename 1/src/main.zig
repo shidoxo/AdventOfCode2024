54b06879c82d63a76e6386e9aa2882de39caf773
shidoxo <shidoxo@protonmail.com>
2024-12-01 17:57:35 +0100

    Day 1: Complete

--- 1/src/main.zig
+++ 1/src/main.zig
@@ -1,37 +1,72 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const file = try std.fs.cwd().openFile("input.txt", .{});
+    defer file.close();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var buf_reader = std.io.bufferedReader(file.reader());
+    var in_stream = buf_reader.reader();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    try bw.flush(); // Don't forget to flush!
-}
+    defer {
+        const deinit_status = gpa.deinit();
+        if (deinit_status == .leak) @panic("Memory leak detected!");
+    }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    const contents = try in_stream.readAllAlloc(allocator, std.math.maxInt(usize));
+    defer allocator.free(contents);
 
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+    var col1 = std.ArrayList(u32).init(allocator);
+    defer col1.deinit();
+    var col2 = std.ArrayList(u32).init(allocator);
+    defer col2.deinit();
+
+    var firstAnswer: u32 = 0;
+
+    var line_iter = std.mem.splitScalar(u8, contents, '\n');
+
+    while (line_iter.next()) |line| {
+        if (line.len == 0) continue;
+
+        var column_iter = std.mem.splitSequence(u8, line, "   ");
+
+        if (column_iter.next()) |first| {
+            const num1 = try std.fmt.parseInt(u32, first, 10);
+
+            try col1.append(num1);
+
+            if (column_iter.next()) |second| {
+                const num2 = try std.fmt.parseInt(u32, second, 10);
+
+                try col2.append(num2);
+            }
         }
-    };
-    try std.testing.fuzz(global.testOne, .{});
+    }
+
+    std.mem.sort(u32, col1.items, {}, comptime std.sort.asc(u32));
+    std.mem.sort(u32, col2.items, {}, comptime std.sort.asc(u32));
+
+    for (col1.items, col2.items) |num1, num2| {
+        const diff = if (num1 > num2) num1 - num2 else num2 - num1;
+        firstAnswer += diff;
+    }
+
+    std.debug.print("Answer to the first question: {}\n", .{firstAnswer});
+
+    var secondAnswer: u32 = 0;
+
+    for (col1.items) |num1| {
+        var count: u32 = 0;
+        for (col2.items) |num2| {
+            if (num1 == num2) {
+                count += 1;
+            }
+        }
+
+        secondAnswer += num1 * count;
+    }
+
+    std.debug.print("Answer to the second question: {}\n", .{secondAnswer});
 }
